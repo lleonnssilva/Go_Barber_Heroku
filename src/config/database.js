2ca874617e19ee3581997a98120fc73f46546e3a
lleonnssilva <leoguaruleo@gmail.com>
2019-06-07 09:27:10 -0300

    Primeito commit

---
+++ src/config/database.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // dialect: "postgres",
+  // host: "127.0.0.1",
+  // username: "postgres",
+  // password: "postgres",
+  // database: "go_barber",
+  // operationAliases: false,
+  // define: {
+  //   timestamp: true,
+  //   underscored: true,
+  //   underscoredAll: true
+  // }
+
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: true
+  }
+};
