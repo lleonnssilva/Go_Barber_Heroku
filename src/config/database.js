3366b367869b3a7675812b2ce38f65649137fd13
lleonnssilva <leoguaruleo@gmail.com>
2019-06-07 09:48:56 -0300

    teste migration

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,34 @@
-module.exports = {
-  // dialect: "postgres",
-  // host: "127.0.0.1",
-  // username: "postgres",
-  // password: "postgres",
-  // database: "go_barber",
-  // operationAliases: false,
-  // define: {
-  //   timestamp: true,
-  //   underscored: true,
-  //   underscoredAll: true
-  // }
+// module.exports = {
+//   // dialect: "postgres",
+//   // host: "127.0.0.1",
+//   // username: "postgres",
+//   // password: "postgres",
+//   // database: "go_barber",
+//   // operationAliases: false,
+//   // define: {
+//   //   timestamp: true,
+//   //   underscored: true,
+//   //   underscoredAll: true
+//   // }
 
-  dialect: "postgres",
-  protocol: "postgres",
-  dialectOptions: {
-    ssl: true
+//   dialect: "postgres",
+//   protocol: "postgres",
+//   dialectOptions: {
+//     ssl: true
+//   }
+// };
+const fs = require("fs");
+
+module.exports = {
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: true
+    }
   }
 };
